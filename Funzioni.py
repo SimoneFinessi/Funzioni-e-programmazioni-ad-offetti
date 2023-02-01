a7588c1c61b8bc13bd67e09c09885c21aa4f7adf
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-01 12:28:56 +0000

    spiegazione+esempio

--- Funzioni.py
+++ Funzioni.py
@@ -1,3 +1,11 @@
 #i print()sono funzioni infatti ne abbiamo gia usate alcune
 print("ciao")#funzione integrata
 #FUNZIONE= blocco di istruzioni utilizzate per eseguire un'attivita specifica
+#perche non usare copia-incolla
+#perceh usando la funzine rendi il codice piu compatto e leggibile, rendendolo anche facilmente modificabile in caso di errori+
+#si creano con def <nome()>: 
+#dentro alle parentesi ci inseriamo i parametri
+def nome():#dichiaro la funzione e il nome
+    name=input("inserisci nome")
+    print(name)
+nome()#richiamo la funzione

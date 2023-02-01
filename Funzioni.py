bc852c4c7df0c661c94b2ff4b70d0004dca95f1e
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-01 12:36:53 +0000

    aggiunta valori

--- Funzioni.py
+++ Funzioni.py
@@ -9,3 +9,15 @@ def nome():#dichiaro la funzione e il nome
     name=input("inserisci nome")
     print(name)
 nome()#richiamo la funzione
+def addizione(a,b):#PARAMETRO modo che l'utente a per passare valori tra le funzioni
+    risultato=a+b
+    print(str(risultato))
+addizione(15,5)#inserisco oltre al richiamare la funzione anche i valori il primo sara poi chiamato A e il second B
+
+
+#si puo rendere il passaggio di tutti i valori opzionali dando un valore di dafoult ai valori nella funzione 
+def addnoval(a,b=0):
+    print(a,b)
+addnoval(1)
+addnoval(1,2)
+

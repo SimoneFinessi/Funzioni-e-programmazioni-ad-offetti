d680dbed203080ac7dd00ccbe6d993f807d1adea
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-07 11:50:34 +0000

    es funzioni

---
+++ esercizzi2.py
@@ -0,0 +1,29 @@
+#scrivere un funzione che facci la somma tra due numeri interi
+'''def somma2(a,b):
+    return int(a+b)
+print(somma2(1,2))'''
+
+#utilizzare la funzione precedente per scirivere una funzione che faci la somma di tre numeri interi
+'print(somma2(somma2(1,2),3))
+#scrivere una funzione prodotto  chiamata prodotto che facci il prodotto di due numeri inseriti dall'utente usando la funzione somma usata precedentemente
+'''def prodotto(a,b):
+    x=a 
+    if b <0:
+       c= b*-1
+    for i in range(c-1):
+       x=somma2(x,a)
+    if b<0:
+        x=x*-1
+    return x
+print(prodotto(int(input("")),int(input(""))))'''
+#scrivere la funzione MCD che calcola il massimo comun divisore tra due numeri 
+
+#mcm minimo comune multiplo
+
+#scrivere una funizone chhiamata semplifica che semplifica una frazione
+
+#scrivere una funzione che permetta di fare la somma tra due frazioni dando il risultato semplificato
+
+#scrivere una funzione che permetta di fare il prodotto tra due frazioni dando il risultato semplificato
+
+#scrivere una funzione che permetta di fare la divisione tra due frazioni dando il risultato semplificato
--- funzione_carattere.py
+++ funzione_carattere.py
@@ -1,7 +1,10 @@
 def vocale(a):
-    if a in ["a","e","i","o","u"]:
+    if a.lower() in ["a","e","i","o","u"]:
         voc="si"
     else:
         voc="no"
     return voc
 print(vocale(input("inserisci una lettera minuscola: ")))
+
+#scirvere un programma che dato un carattere dica se si tratta di una vocale o di una consonante
+print("vocale") if vocale(input("inserisci una lettera minuscola: "))=="si" else print("consonante")

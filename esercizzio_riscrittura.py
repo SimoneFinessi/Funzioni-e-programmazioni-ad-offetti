324371a53b8a19c82b8922ed408afc19410fb4e6
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-15 12:38:17 +0000

    eseguito esercizio riscrittura

---
+++ esercizzio_riscrittura.py
@@ -0,0 +1,22 @@
+def controllo(a=""):
+    global x
+    if a=="Dante" or a=="Manzoni":
+        print("corretto")
+        print("bravo")
+    else:
+        if x == 1:
+            print("la risposta é Manzoni")
+        else:
+            print("la risposta é Dante")
+        print("studia di più")
+
+risposta1=input("chi ha scritto i promessi sposi?:")
+x=1
+controllo(risposta1)
+risposta2=input("chi ha scritto la divina commedia?:")
+x=2
+controllo(risposta2)
+
+
+
+        
